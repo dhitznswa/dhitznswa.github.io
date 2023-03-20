aaf42be98ffacadf8780ebcc9e348cd08d552077
dhitznswa <dhitz.nswa@gmail.com>
2023-03-20 16:27:21 +0700

    Upload Portfoliov4

---
+++ dist/jscript/animasi.js
@@ -0,0 +1,114 @@
+gsap.from(".navbar-brand", {
+  opacity: 0,
+  x: -100,
+  duration: 2,
+  ease: "bounce",
+});
+
+gsap.from("#nav-about", {
+  opacity: 0,
+  y: 100,
+  duration: 2,
+  ease: "back",
+});
+
+gsap.from("#nav-skils", {
+  opacity: 0,
+  y: 100,
+  duration: 2,
+  delay: 0.5,
+  ease: "back",
+});
+
+gsap.from("#nav-project", {
+  opacity: 0,
+  y: 100,
+  duration: 2,
+  delay: 1,
+  ease: "back",
+});
+
+gsap.from("#nav-performance", {
+  opacity: 0,
+  y: 100,
+  duration: 2,
+  delay: 1.5,
+  ease: "back",
+});
+
+gsap.from("#nav-contact", {
+  opacity: 0,
+  y: 100,
+  duration: 2,
+  delay: 2,
+  ease: "back",
+});
+
+gsap.from("#text-header-atas", {
+  opacity: 0,
+  y: -100,
+  duration: 1,
+});
+
+gsap.from("#text-header-tengah", {
+  opacity: 0,
+  x: -100,
+  duration: 1,
+  delay: 1,
+  ease: "back",
+});
+
+gsap.from("#text-header-bawah", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+});
+
+gsap.from("#text-header-deskripsi", {
+  opacity: 0,
+  x: 100,
+  delay: 1,
+  duration: 2,
+});
+
+gsap.from("#sm-facebook", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  delay: 2.5,
+  ease: "bounce",
+});
+gsap.from("#sm-twitter", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  delay: 2,
+  ease: "bounce",
+});
+gsap.from("#sm-instagram", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  delay: 1.5,
+  ease: "bounce",
+});
+gsap.from("#sm-telegram", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  delay: 1,
+  ease: "bounce",
+});
+gsap.from("#btnDownloadCV", {
+  opacity: 0,
+  y: 100,
+  duration: 2,
+  delay: 3,
+  ease: "back",
+});
+gsap.from("#header-img", {
+  opacity: 0,
+  scale: 0,
+  duration: 3,
+  ease: "back",
+});
---
+++ dist/jscript/main.js
@@ -0,0 +1,9 @@
+function navopen() {
+  const NavContent = document.querySelector("#nav-content");
+  NavContent.classList.toggle("hidden");
+}
+
+async function backsound() {
+  const audio = await new Audio("dist/audio/backsound.mp3");
+  audio.play();
+}
